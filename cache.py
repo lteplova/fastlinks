557846abf7e2abe0032219c80a311d51e0d5ffe5
lteplova <38242392+lteplova@users.noreply.github.com>
2025-03-25 21:59:39 +0300

    Update cache.py
    fixes

--- cache.py
+++ cache.py
@@ -1,33 +1,39 @@
-import redis
-from config import REDIS_HOST, REDIS_PORT
+import json
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    decode_responses=True
-)
-
-# достает из кэша ссылку
-def get_cached_url(short_code):
-    short_code = str(short_code)
-    return redis_client.get(short_code)
 
 # кэширует
-def cache_url(short_code, original_url):
-    short_code = str(short_code)
-    original_url = str(original_url)
-    redis_client.setex(short_code, 3600, original_url)
+def create_cache_url(short_code, original_url, clicks, expires_at, redis_client, last_accessed=0):
 
-def delete_cached_link(short_code):
+    data = {
+        'original_url': original_url,
+        'short_code': short_code,
+        'clicks': clicks,
+        'expires_at': str(expires_at),
+        'last_accessed': str(last_accessed)
+    }
+
+    json_data = json.dumps(data)
+    print(json_data)
+
+    redis_client.setex(short_code, 3600, json_data)
+
+
+# забирает из кэша
+def get_cached_url(short_code, redis_client):
+    short_code = str(short_code)
+    cached_data = redis_client.get(short_code)
+
+    if cached_data is not None:
+        try:
+            data = json.loads(cached_data)
+            return data
+        except json.JSONDecodeError:
+            return None
+    else:
+
+        return None
+
+# удаляет из кэша
+def delete_cached_link(short_code, redis_client):
+    print(short_code)
     redis_client.delete(short_code)
-
-# достает из кэша короткую ссылку
-def get_cached_link(original_url):
-    original_url = str(original_url)
-    return redis_client.get(original_url)
-
-# кэширует
-def cache_link(original_url, short_code):
-    short_code = str(short_code)
-    original_url = str(original_url)
-    redis_client.setex(original_url, 3600, short_code)
