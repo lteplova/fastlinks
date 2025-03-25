64360b610eab343cf4c6672e5f0dbf26d7a57b06
lteplova <38242392+lteplova@users.noreply.github.com>
2025-03-25 21:48:04 +0300

    Update links.py
    fixes

--- routers/links.py
+++ routers/links.py
@@ -6,13 +6,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.responses import RedirectResponse
 from typing import List
 from pydantic import HttpUrl
-from auth.schemas import LinkCreate, LinkUpdate, LinkResponse, LinkStatistics, CustomLinkCreate
+from auth.schemas import LinkCreate, LinkUpdate, LinkResponse, LinkStatistics, CustomAlias
 from dateutil.relativedelta import relativedelta
-from services import (create_short_url, delete_short_url, get_original_url, update_short_url,
-                      update_link_statistics, get_link_stats, create_custom_short, check_alias_uniq, search_links)
+from services import (create_short_url, delete_short_url, update_short_url, get_original_url,
+                      update_link_statistics, get_link_stats, create_custom_short, check_alias_uniq, search_short)
 from auth.users import get_current_user
 from config import REDIS_HOST, REDIS_PORT
-from cache import get_cached_url, cache_url, delete_cached_link, cache_link, get_cached_link
+from cache import get_cached_url, create_cache_url,delete_cached_link
+
 
 router = APIRouter()
 
@@ -21,7 +22,6 @@ redis_client = redis.Redis(
     port=REDIS_PORT,
     decode_responses=True
 )
-
 
 @router.post("/shorten",
              summary="Создать короткую ссылку",
@@ -33,19 +33,19 @@ async def shorten_link(
         db: AsyncSession = Depends(get_async_session),
         current_user=Depends(get_current_user)
 ):
-    cached_data = get_cached_url(link.custom_alias)
+    cached_data = get_cached_url(link.custom_alias, redis_client)
 
     if cached_data:
         return LinkResponse(
-            id=cached_data,
-            original_url=link.original_url,
-            short_code=link.custom_alias,
-            clicks=0,
-            created_at=link.created_at,
-            expires_at=link.expires_at
+            id=None,
+            original_url=cached_data['original_url'],
+            short_code=cached_data['short_code'],
+            clicks=cached_data['clicks'],
+            created_at=None,
+            expires_at=cached_data['expires_at'],
         )
-
     expires_at = link.expires_at.replace(tzinfo=timezone.utc) + relativedelta(months=1)
+
 
     short_url = await create_short_url(
         db,
@@ -55,9 +55,14 @@ async def shorten_link(
         expires_at=expires_at
     )
 
-    cache_url(short_url.short_code, short_url.original_url)
-    cache_link(short_url.original_url, short_url.short_code)
 
+    create_cache_url(
+        short_url.short_code,
+        short_url.original_url,
+        short_url.clicks,
+        short_url.expires_at,
+        redis_client
+    )
 
     return LinkResponse(
         id=short_url.id,
@@ -69,58 +74,8 @@ async def shorten_link(
     )
 
 
-@router.get("/{short_code}",
-            summary="Перенаправить на оригинальный адрес",
-            description="Этот эндпоинт перенаправляет на оригинальный URL по указанной короткой ссылке",
-            )
-async def redirect_to_original(
-        short_code: str,
-        db: AsyncSession = Depends(get_async_session)
-):
-    # проверка кэша
-    cached_url = get_cached_url(short_code)
-    if cached_url:
-        return RedirectResponse(url=cached_url)
-
-    link = await get_original_url(db, short_code)
-
-    if not link:
-        raise HTTPException(status_code=404, detail="Ссылка не найдена")
-
-    now = datetime.now(timezone.utc)
-    if not link.expires_at:
-        if link.created_at:
-            expiration_date = link.created_at + relativedelta(months=1)
-            if now > expiration_date:
-                raise HTTPException(status_code=410, detail="Срок действия ссылки истек")
-    elif link.expires_at < now:
-        raise HTTPException(status_code=410, detail="Срок действия ссылки истек")
-
-    await update_link_statistics(db, link.id)
-
-    return RedirectResponse(url=link.original_url)
 
 
-@router.delete("/{short_code}",
-               summary="Удаление ссылки",
-               description="Этот эндпоинт удаляет короткую ссылку",
-               status_code=status.HTTP_204_NO_CONTENT)
-async def delete_link(
-        short_code: str,
-        db: AsyncSession = Depends(get_async_session),
-        current_user=Depends(get_current_user)
-):
-    deleted = await delete_short_url(db, short_code, current_user.id)
-
-    if deleted:
-        delete_cached_link(short_code)
-        return None
-
-
-    raise HTTPException(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail="Произошла неизвестная ошибка при удалении ссылки"
-    )
 
 @router.put("/{short_code}",
             summary="Обновление короткой ссылки",
@@ -149,8 +104,15 @@ async def update_link(
         expires_at=expires_at
     )
 
-    delete_cached_link(short_code)
-    cache_url(updated_link.short_code, updated_link.original_url)
+    delete_cached_link(short_code, redis_client)
+
+    create_cache_url(
+        updated_link.short_code,
+        updated_link.original_url,
+        updated_link.clicks,
+        updated_link.expires_at,
+        redis_client
+    )
 
     return LinkResponse(
         id=updated_link.id,
@@ -165,39 +127,50 @@ async def update_link(
             summary="Вывод статистики использования короткой ссылки",
             description="Этот эндпоинт показывает сколько раз кликали на короткую ссылку и время последнего клика",
             response_model=LinkStatistics)
+
 async def get_link_statistics(
         short_code: str,
         db: AsyncSession = Depends(get_async_session)
 ):
-    stats = await get_link_stats(db, short_code)
+    cached = get_cached_url(short_code, redis_client)
 
-    if not stats:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Ссылка с кодом '{short_code}' не найдена"
+    if cached:
+        return LinkStatistics(
+            original_url=cached['original_url'],
+            short_code=cached['short_code'],
+            clicks=cached['clicks'],
+            last_accessed=cached['last_accessed'],
+            expires_at=cached['expires_at']
         )
-    if stats.clicks == 0:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"Не было переходов"
+    else:
+        stats = await get_link_stats(db, short_code)
+
+        if not stats:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Ссылка с кодом '{short_code}' не найдена"
+            )
+        if stats.clicks == 0:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Не было переходов"
+            )
+
+        return LinkStatistics(
+            original_url=stats.original_url,
+            short_code=stats.short_code,
+            clicks=stats.clicks,
+            last_accessed=stats.last_accessed,
+            expires_at=stats.expires_at
         )
 
-    return LinkStatistics(
-        original_url=stats.original_url,
-        short_code=stats.short_code,
-        created_at=stats.created_at,
-        clicks=stats.clicks,
-        last_accessed=stats.last_accessed,
-        expires_at=stats.expires_at
-    )
-
-#
 @router.post("/shorten/custom",
              summary="Изменение короткой ссылки и времени",
              description="Этот эндпоинт позволяет задать кастомный алиас для ссылки и изменить время жизни существующей ссылки",
              response_model=LinkResponse)
+
 async def create_custom_link(
-        link: CustomLinkCreate,
+        link: CustomAlias,
         db: AsyncSession = Depends(get_async_session),
         current_user=Depends(get_current_user)
 ):
@@ -237,6 +210,10 @@ async def create_custom_link(
         expires_at=expires_at
     )
 
+    delete_cached_link(link.short_code, redis_client)
+
+    create_cache_url(short_url.custom_alias, short_url.original_url, 0, short_url.expires_at, redis_client)
+
     return LinkResponse(
         id=short_url.id,
         original_url=short_url.original_url,
@@ -251,28 +228,14 @@ async def create_custom_link(
             summary="Поиск короткой ссылки",
             description="Этот эндпоинт позволяет найти короткую ссылку в БД по оригинальному URL",
             response_model=List[LinkResponse])
-async def search_links_by_url(
-        original_url: HttpUrl = Query(..., description="Оригинальный URL для поиска"),
-        db: AsyncSession = Depends(get_async_session)
+async def search_by_url(
+        url: HttpUrl = Query(..., description="Оригинальный URL для поиска"),
+        # current_user=Depends(get_current_user),
+        db: AsyncSession = Depends(get_async_session),
+
 ):
-    cached_short = get_cached_link(original_url)
 
-    if cached_short:
-
-        response = [
-                    LinkResponse(
-                        id=link.id,
-                        original_url=link.original_url,
-                        short_code=cached_short,
-                        clicks=link.clicks,
-                        created_at=link.created_at,
-                        expires_at=link.expires_at,
-                    )
-                    for link in links
-                ]
-        return response
-
-    links = await search_links(db, str(original_url))
+    links = await search_short(db, str(url))
 
     if not links:
         raise HTTPException(status_code=404, detail="Ссылки не найдены")
@@ -290,3 +253,61 @@ async def search_links_by_url(
     ]
 
     return response
+
+@router.get("/{short_code}",
+            summary="Перенаправить на оригинальный адрес",
+            description="Этот эндпоинт перенаправляет на оригинальный URL по указанной короткой ссылке",
+            )
+async def redirect_to_original(
+        short_code: str,
+        db: AsyncSession = Depends(get_async_session),
+):
+    # проверка кэша
+    cached_url = get_cached_url(short_code, redis_client)
+    now = datetime.now(timezone.utc)
+
+    if cached_url:
+        create_cache_url(short_code, cached_url['original_url'], cached_url['clicks']+1, cached_url['expires_at'], redis_client, now)
+        await update_link_statistics(db, short_code)
+        return RedirectResponse(url=cached_url['original_url'])
+    else:
+        link = await get_original_url(db, short_code)
+
+        if not link:
+            raise HTTPException(status_code=404, detail="Ссылка не найдена")
+
+        if not link.expires_at:
+            if link.created_at:
+                expiration_date = link.created_at + relativedelta(months=1)
+                if now > expiration_date:
+                    raise HTTPException(status_code=410, detail="Срок действия ссылки истек")
+        elif link.expires_at < now:
+            raise HTTPException(status_code=410, detail="Срок действия ссылки истек")
+
+        await update_link_statistics(db, link.short_code)
+
+        create_cache_url(short_code, link.original_url, link.clicks, link.expires_at, redis_client, now)
+
+        return RedirectResponse(url=link.original_url)
+
+
+@router.delete("/{short_code}",
+               summary="Удаление ссылки",
+               description="Этот эндпоинт удаляет короткую ссылку",
+               status_code=status.HTTP_204_NO_CONTENT)
+async def delete_link(
+        short_code: str,
+        db: AsyncSession = Depends(get_async_session),
+        current_user=Depends(get_current_user)
+):
+    deleted = await delete_short_url(db, short_code, current_user.id)
+
+    if deleted:
+        delete_cached_link(short_code, redis_client)
+        return None
+
+
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Произошла неизвестная ошибка при удалении ссылки"
+    )
