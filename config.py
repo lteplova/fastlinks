de4ced75cc247d35444bfa212608c4469a86dd3a
lteplova <38242392+lteplova@users.noreply.github.com>
2025-03-26 12:59:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
-APP_PORT = s.getenv("APP_PORT")
+APP_PORT = os.getenv("APP_PORT")
 
 DATABASE_URL_A = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
